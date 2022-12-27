9f863179c38faa7f0139d9aa6d093d9262a41842
AhmedHall <omdadesginer@gmail.com>
2022-12-27 16:01:34 +0200

    update response url

--- client/script.js
+++ client/script.js
@@ -79,7 +79,7 @@ const handelSubmit = async (e) => {
 
   //fetch data from server
 
-  const response = await fetch('http://localhost:5000', {
+  const response = await fetch('https://bixa.onrender.com', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
